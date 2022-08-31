fdd2e5bc1b6f3e7fb97065b999243089b88413a9
leolopeso <leonardo.lopes@corpstek.com.br>
2022-08-31 09:19:48 -0300

    Criando rota post e enviando parâmetros

--- src/server.js
+++ src/server.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
+// GET -> SOLICITAR DADOS
+// POST -> CADASTRA DADOS
+// PUT -> ATUALIZA DADOS
+// DELETE -> DELETAR DADOS
+
 app.use(express.json());
 
-app.listen(3333, () => {
-    console.log("Servidor levantado.");
-});
+app.get('/', (request, response) => { 
+    return response.json({ message: "Api online" });
+ });
+
+app.post('/', (request, response) => {
+    const { valor1, valor2  } = request.body;
+
+    const total = valor1 + valor2;
+
+    return response.json(total);
+})
+
+app.listen(3333, () => console.log("Server only."));
